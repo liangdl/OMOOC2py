edee8700a9d40e72119099c4255d2fc85f42bff6
liangdl <liangdlc@gmail.com>
2015-10-18 11:09:12 -0400

    Done

---
+++ _src/om2py0w/0wex1/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from sys import argv
+
+# display journal.txt content
+print "之前输入的日志："
+print open("journal.txt").read()
+
+#append new message to journal.txt
+target = open("journal.txt", 'a')
+line = raw_input(">")
+target.write(line)
+target.write("\n")
+target.close()
