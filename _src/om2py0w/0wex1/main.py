5f4d8d6b6dd314dc1c359787900c68630b7a2eb6
liangdl <liangdlc@gmail.com>
2015-10-22 13:19:50 -0400

    一大半

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from sys import argv
+import sys
 
-def new_journal():
+# display journal.txt
+def display_journal():
     # display journal.txt content
     print "之前输入的日志："
     print open("journal.txt").read()
 
+
+def new_journal():
+    # display journal.txt content
+    display_journal()
     #append new message to journal.txt
     target = open("journal.txt", 'a')
     line = raw_input(">")
     target.write(line)
     target.write("\n")
     target.close()
+
+
+display_journal()
